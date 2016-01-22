835e6e123682bdfdd38b0d1b2397c15148e9e58f
Anna Lawley <alawley1@gmail.com>
2016-01-22 13:34:15 -0800

    rewrite pingpong tests to account for prime input

--- specs/specs.js
+++ specs/specs.js
@@ -11,11 +11,20 @@ describe ('pingPong', function () {
   it("will return 'pingpong' instead if the input number is evenly divisble by 3 and 5", function () {
     expect(pingPong(15)).to.equal("pingpong");
   });
+  it("will return 'number - prime' for prime numbers", function () {
+    expect(pingPong(7)).to.equal("7 - prime");
+  });
+  it("will return 'ping - prime' instead for 3", function () {
+    expect(pingPong(3)).to.equal("ping - prime");
+  });
+  it("will return 'pong - prime' instead for 5", function () {
+    expect(pingPong(5)).to.equal("pong - prime");
+  });
 });
 
 describe ('pingPongLoop', function() {
   it("will aggregate into an array all pingPong results from 1 to the input number", function() {
-    expect(pingPongLoop(15)).to.eql([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "pingpong"]);
+    expect(pingPongLoop(15)).to.eql([1, "2 - prime", "ping - prime", 4, "pong - prime", "ping", "7 - prime", 8, "ping", "pong", "11 - prime", "ping", "13 - prime", 14, "pingpong"]);
   });
   it("will handle negative numbers appropraitely", function() {
     expect(pingPongLoop( -15)).to.eql([-1, -2, "ping", -4, "pong", "ping", -7, -8, "ping", "pong", -11, "ping", -13, -14, "pingpong"]);
@@ -42,7 +51,7 @@ describe ('contains', function() {
 })
 
 describe ('sieve', function() {
-  it("will give array of all primes up to number", function () {
+  it("will give array of all primes up to input number", function () {
     expect(sieve(23)).to.eql([2, 3, 5, 7, 11, 13, 17, 19, 23]);
   });
 });
