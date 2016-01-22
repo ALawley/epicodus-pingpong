1df27c996f827cf2e039e4f3b3613720e0fdeb1e
Anna Lawley <alawley1@gmail.com>
2016-01-22 08:41:24 -0800

    add pong handling

--- specs/specs.js
+++ specs/specs.js
@@ -5,4 +5,7 @@ describe ('pingPong', function () {
   it("will return 'ping' if the input number is evenly divisble by 3", function () {
     expect(pingPong(9)).to.equal("ping");
   });
+  it("will return 'pong' if the input number is evenly divisble by 5", function () {
+    expect(pingPong(10)).to.equal("pong");
+  });
 });
