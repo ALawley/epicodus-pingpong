9e4b523b5313f8e6b643b518e8c247413162eba1
Anna Lawley <alawley1@gmail.com>
2016-01-22 08:39:58 -0800

    add ping handling

--- specs/specs.js
+++ specs/specs.js
@@ -2,4 +2,7 @@ describe ('pingPong', function () {
   it("will return the input number", function () {
     expect(pingPong(8)).to.equal(8);
   });
+  it("will return 'ping' if the input number is evenly divisble by 3", function () {
+    expect(pingPong(9)).to.equal("ping");
+  });
 });
